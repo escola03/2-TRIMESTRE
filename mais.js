d242722a300c2b26869bb506a30742b4627c227d
escola03 <132581680+escola03@users.noreply.github.com>
2024-07-01 08:54:52 -0300

    Add files via upload

---
+++ mais.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".CAIXA-PRINCIPAL")
+const caixaPerguntas = document.querySelector(".Caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: ["alternativa 1",
+            "alternativa 2"],
+    },
+
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativas 1",
+            "Alternativa 2"]
+    },
+
+]
